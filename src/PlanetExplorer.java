a7ef14e2d2216a6d03bcdda27a9b94d6f97a712d
somename <someemail>
2016-10-20 15:38:54 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -69,7 +69,6 @@ public class PlanetExplorer {
 				break;
 			}
 		}
-		return "(" + explorer.getX() + explorer.getY() + explorer.getDirection() + ")";
-		return null;
+		return "(" + explorer.getX() +","+ explorer.getY() +","+ explorer.getDirection() + ")";
 	}
 }
