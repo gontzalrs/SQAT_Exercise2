65ac777d16493ca036777660e5ead7013c2b45a7
somename <someemail>
2016-10-20 15:04:12 +0300

    besouro automatic message

--- src/Explorer.java
+++ src/Explorer.java
@@ -2,10 +2,13 @@
 public class Explorer {
 	private int x;
 	private int y;
+	private int direction;
 	
-	private enum direction{
-		N, E, S, W
-	}
+	final int N = 0;
+	final int E = 1;
+	final int S = 2;
+	final int W = 3;
+	
 	public Explorer() {
 		x = 0;
 		y = 0;
